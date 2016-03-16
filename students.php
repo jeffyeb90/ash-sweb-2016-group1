e28a900cb098b79495710364ee441c36e4eec58a
makafuifie <fiemakafui2013@gmail.com>
2016-03-16 22:26:43 +0000

    getStudents function

--- students.php
+++ students.php
@@ -6,8 +6,13 @@
 			
 		}
 
-		function getStudents($){
+		function getStudents($filter=false){
+			$strQuery= "select STUDENTID, USERNAME, FIRSTNAME, LASTNAME, PASSWORD, GENDER, EMAIL, PHONENUMBER, EMERGENCYCONTACTID from students";
 
+			if($filter!=false){
+				$strQuery=$strQuery . " where $filter";
+			}
+			return $this->query($strQuery);
 		}
 
 	}
