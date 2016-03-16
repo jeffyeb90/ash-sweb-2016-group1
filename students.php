961f5864f7b02cd77ce4a5fb76ee650f0be76a80
Jeffrey Takyi-Yeboah <JeffreyTakyi-Yeboah@Jeffreys-MacBook-Pro.local>
2016-03-16 23:50:47 +0000

    Commit with addStudentRecord()

--- students.php
+++ students.php
@@ -5,8 +5,24 @@
 		function students(){
 
 		}
+		/**
+		*Adds a new user
+		*@param int studentID
+		*@param decimal weight weight of student
+		*@param decimal height height of student
+		*@param string bloodtype bloodtype of student
 
-		function addStudentRecord(){}
+		*@return boolean returns true if successful or false
+		*/
+		function addStudentRecord($studentID,$weight,$height,$bloodtype){
+			$strQuery="insert into studentHasRecord set
+							STUDENTID='$studentID',
+							HEIGHT='$height',
+							WEIGHT='$weight',
+							BLOODTYPE=''$bloodtype'";
+			return $this->query($strQuery);
+		}
+
 
 	}
 	?>
