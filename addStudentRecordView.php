bdaa8590be3cce6c53cbae60d6b155a9f586f6b8
Jeffrey Takyi-Yeboah <JeffreyTakyi-Yeboah@Jeffreys-MacBook-Pro.local>
2016-03-17 00:44:15 +0000

    Interface updated

--- addStudentRecordView.php
+++ addStudentRecordView.php
@@ -3,20 +3,37 @@
 		<title>Add Student Record</title>
 		<link rel="stylesheet" href="css/style.css">
 		<script>
-			<!--add validation js script here
+
 		</script>
 	</head>
 	<body>
-<div>
-<form action="" method="GET">
-			<div>Student ID: <input type="text" name="studentID" value=""/></div>
-			<div>Weight: <input type="text" name="weight" value=""/>
-			<div>Height: <input type="text" name="height" value=""/>
-			<div>Bloodtype: <input type="bloodtype" name=""/></div>
 
-			<input type="submit" value="Add">
-		</form>
+		<table>
+			<tr>
+				<td colspan="2" id="pageheader">
+				Add Student Record
+				</td>
+			</tr>
+			<tr>
+				<td id="mainnav">
+					<div class="menuitem">menu 1</div>
+					<div class="menuitem">menu 2</div>
+				</td>
+				<td id="content">
+					<div id="divPageMenu">
+						<span class="menuitem" >page menu 1</span>
+						<input type="text" id="txtSearch" />
+						<span class="menuitem">search</span>
 					</div>
-
-	</body>
-</html>
+          <div id="divStatus" class="status">
+          					</div>
+          					<div id="divContent">
+          						Content space
+                      <form action="" method="GET">
+            			<div>StudentID: <input type="text" name="studentID" value=""/></div>
+            			<div>Height: <input type="text" name="weight" value=""/>
+            			<div>Weight: <input type="text" name="height" value=""/>
+            			<div>Bloodtype: <input type="bloodtype" name="bloodtype"/></div>
+            			<input type="submit" value="Add">
+          						</form>
+          					</div>
