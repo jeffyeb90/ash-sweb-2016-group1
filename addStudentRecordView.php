1b68d6467c7d68194522e35c407c7c00881b2e71
Jeffrey Takyi-Yeboah <JeffreyTakyi-Yeboah@Jeffreys-MacBook-Pro.local>
2016-03-17 00:52:15 +0000

    New Changes

--- addStudentRecordView.php
+++ addStudentRecordView.php
@@ -25,15 +25,43 @@
 						<input type="text" id="txtSearch" />
 						<span class="menuitem">search</span>
 					</div>
+          <?php
+          			//initialize
+
+          			$studentID="";
+          			$weight="";
+          			$height="";
+          			$bloodtype=="";
+          				include_once("students.php");
+          			//1) what is the purpose of this if block
+          			if(isset($_REQUEST['studentID'])){
+          				$studentID=$_REQUEST['studentID'];
+          				$weight=$_REQUEST['weight'];
+          				$height = $_REQUEST['height'];
+          				$bloodtype=$_REQUEST['bloodtype'];
+          			]
+
+
+          				$obj=new students();
+          				$r=$obj->addStudentRecord($studentID,$weight,$height,$bloodtype);
+          				//1) what is the purpose of this if block
+          				if($r==false){
+          					$strStatusMessage="error while adding student information";
+          				}else{
+          					$strStatusMessage=" Student with ID $studentID added";
+          				}
+
+          			}
+          ?>
           <div id="divStatus" class="status">
           					</div>
           					<div id="divContent">
           						Content space
                       <form action="" method="GET">
-            			<div>StudentID: <input type="text" name="studentID" value=""/></div>
-            			<div>Height: <input type="text" name="weight" value=""/>
-            			<div>Weight: <input type="text" name="height" value=""/>
-            			<div>Bloodtype: <input type="bloodtype" name="bloodtype"/></div>
+            			<div>StudentID: <input type="text" name="studentID" value="<?php echo $studentID  ?>"/></div>
+            			<div>Height: <input type="text" name="weight" value="<?php echo $weight  ?>"/>
+            			<div>Weight: <input type="text" name="height" value="<?php echo $height  ?>"/>
+            			<div>Bloodtype: <input type="bloodtype" name="bloodtype"  value="<?php echo $bloodtype  ?>"/></div>
             			<input type="submit" value="Add">
           						</form>
           					</div>
