d8e6fbde4c29a419af0e00ab305a68a4e07e0c4c
Jeffrey Takyi-Yeboah <JeffreyTakyi-Yeboah@Jeffreys-MacBook-Pro.local>
2016-03-17 01:08:49 +0000

    New changes

--- addStudentRecordView.php
+++ addStudentRecordView.php
@@ -25,6 +25,21 @@
 						<input type="text" id="txtSearch" />
 						<span class="menuitem">search</span>
 					</div>
+          <div id="divStatus" class="status">
+                    </div>
+                    <div id="divContent">
+                      Content space
+                      <form action="" method="GET">
+                  <div>StudentID: <input type="text" name="studentID" value="<?php echo $studentID  ?>"/></div>
+                  <div>Height: <input type="text" name="weight" value="<?php echo $weight  ?>"/>
+                  <div>Weight: <input type="text" name="height" value="<?php echo $height  ?>"/>
+                  <div>Bloodtype: <input type="bloodtype" name="bloodtype"  value="<?php echo $bloodtype  ?>"/></div>
+                  <input type="submit" value="Add">
+                      </form>
+                    </div>
+                  </table>
+                </body>
+              </html>
           <?php
           			//initialize
 
@@ -34,34 +49,23 @@
           			$bloodtype="";
           				include_once("students.php");
           			//1) what is the purpose of this if block
-          			if(isset($_REQUEST['studentID'])){
+          			if(!isset($_REQUEST['studentID'])){
+                  exit();
+                }
+                else{
           				$studentID=$_REQUEST['studentID'];
           				$weight=$_REQUEST['weight'];
           				$height = $_REQUEST['height'];
           				$bloodtype=$_REQUEST['bloodtype'];
 
-
-
           				$obj=new students();
           				$r=$obj->addStudentRecord($studentID,$weight,$height,$bloodtype);
           				//1) what is the purpose of this if block
           				if($r==false){
-          					$strStatusMessage="error while adding student information";
+          					echo "error while adding student information";
           				}else{
-          					$strStatusMessage=" Student with ID $studentID added";
+          					echo" Student with ID $studentID added";
           				}
 
           			}
           ?>
-          <div id="divStatus" class="status">
-          					</div>
-          					<div id="divContent">
-          						Content space
-                      <form action="" method="GET">
-            			<div>StudentID: <input type="text" name="studentID" value="<?php echo $studentID  ?>"/></div>
-            			<div>Height: <input type="text" name="weight" value="<?php echo $weight  ?>"/>
-            			<div>Weight: <input type="text" name="height" value="<?php echo $height  ?>"/>
-            			<div>Bloodtype: <input type="bloodtype" name="bloodtype"  value="<?php echo $bloodtype  ?>"/></div>
-            			<input type="submit" value="Add">
-          						</form>
-          					</div>
