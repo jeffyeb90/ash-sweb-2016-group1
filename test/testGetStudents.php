0ffb5f56b40ddd7f9b3363839c7fb6cb427d27e4
makafuifie <fiemakafui2013@gmail.com>
2016-03-18 16:24:21 +0000

    updates

--- test/testGetStudents.php
+++ test/testGetStudents.php
@@ -4,9 +4,19 @@
 	class testGetStudents extends PHPUnit_Framework_TestCase{
 		public function testGetStudents(){
 			$student = new students();
-			assertEquals(true, $student->getStudents());
+			$this->assertEquals(true, $student->getStudents());
 			$row=$student->fetch();
 			$this->assertGreaterThan(0, count($row));
 		}
+
+	public function testSearchStudents(){
+		$student = new students();
+		$username = "jessie";
+		$this->assertEquals(true, $student->searchStudents($username));
+
+		$row=$student->fetch();
+		$this->assertEquals("jessie.aye", $row['USERNAME']);
+	}
+
 	}
 ?>
