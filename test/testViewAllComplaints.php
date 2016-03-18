2b3676efcb8a9ea4eed0ecb9dc912f0c83d6d030
makafuifie <fiemakafui2013@gmail.com>
2016-03-18 15:55:11 +0000

    tests

---
+++ test/testViewAllComplaints.php
@@ -0,0 +1,9 @@
+<?php
+include_once("..medicalComplaint.php");
+class testViewAllComplaints extends PHPUnit_Framework_TestCase{
+	public function testGetComplaints(){
+		$complaint = new complaint();
+		
+	}
+}
+?>
