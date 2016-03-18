7d189da2ae2b0e6c3559efebaf8826403366f1c4
Andrew Abbeyquaye <andrew.abbeyquaye@icloud.com>
2016-03-18 00:12:01 +0000

    getAllMedicalComplaints function implemented
    This function allows one to get medical complaints of students.

--- medicalComplaint.php
+++ medicalComplaint.php
@@ -5,6 +5,22 @@
 		function medicalComplaint(){
 			
 		}
+        
+        
+        /**
+        *gets medical complaints based on the filter
+        *@param string mixed condition to filter. If  false, then filter will not be applied
+        *@return boolean true if successful, else false
+        */
+        function getAllMedicalComplaints($filter=false){
+            $strQuery="select COMPLAINTID, students.STUDENTID, students.FIRSTNAME, students.LASTNAME, DATE,TEMPERATURE,SYMPTOMS,DIAGNOSIS,CAUSE,PRESCRIPTION,NURSEID from students left join medicalComplaint on students.STUDENTID=medicalComplaint.STUDENTID";
+            
+            if($filter!=false){
+                $strQuery=$strQuery . " where $filter";
+            }
+            $strQuery=$strQuery . " ORDER BY DATE DESC";
+            return $this->query($strQuery);
+        }
 
 	}
 ?>
