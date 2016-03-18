a74a74fb471292be41f48ae313483f5be9e5d858
makafuifie <fiemakafui2013@gmail.com>
2016-03-18 14:01:29 +0000

    search function

--- medicalComplaint.php
+++ medicalComplaint.php
@@ -5,9 +5,14 @@
 		function medicalComplaint(){
 			
 		}
+		/**
+		*get complaint records based on the filter
+		*@param string condition to filter. If false, filter will not be applied
+		*@return boolean true if successful, false if unsuccessful
+		**/
 
 		function getComplaints($filter=false){
-			 $strQuery="select COMPLAINTID, STUDENTID, DATE, TEMPERATURE, SYMPTOMS, diseases.NAME, CAUSE, PRESCRIPTION, nurses.FIRSTNAME, nurses.LASTNAME from medicalcomplaint, diseases, nurses where medicalcomplaint.DIAGNOSIS=diseases.DISEASEID and medicalcomplaint.NURSEID=nurses.NURSEID";
+			 $strQuery="select COMPLAINTID, STUDENTID, DATE, TEMPERATURE, SYMPTOMS, diseases.NAME, CAUSE, PRESCRIPTION, nurses.FIRSTNAME, nurses.LASTNAME from medicalcomplaint left join diseases on medicalcomplaint.DIAGNOSIS=diseases.DISEASEID left join nurses on  medicalcomplaint.NURSEID=nurses.NURSEID";
 
 	
 
@@ -17,13 +22,19 @@
 			}
 			return $this->query($strQuery); 
 		}
+		/**
+		*search for medical complaints using student ID as a filter
+		*@param string student ID to filter by. If false, calls getComplaints function to display all complaints
+		*@return boolean true if successful, false if unsuccessful
+		**/
+		function searchComplaintsByStudentID($text=false){
+			$filter=false;
+			if($text!=false){
+				$filter=" STUDENTID like '$text'";
+			}
 
-		// function searchComplaints($text=false){
-		// 	$filter=false;
-		// 	if($text!=false){
-		// 		$filter=" "
-		// 	}
-		// }
+			return $this->getComplaints($filter);
+		}
 
 
 
