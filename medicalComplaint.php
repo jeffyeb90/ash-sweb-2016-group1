4e5bf9bca490ac4daaed8a37036e81bdc1d3fd76
makafuifie <fiemakafui2013@gmail.com>
2016-03-25 02:50:30 +0000

    update complaints function

--- medicalComplaint.php
+++ medicalComplaint.php
@@ -6,5 +6,10 @@
 			
 		}
 
+		function updateComplaints($complaintID, $studentID, $temperature, $symptoms, $diagnosis, $cause, $prescription, $nurseID){
+				$strQuery="Update medicalcomplaint set STUDENTID=$studentID,  TEMPERATURE=$temperature, SYMPTOMS='$symptoms', DIAGNOSIS='$diagnosis', CAUSE='$cause', PRESCRIPTION='$prescription', NURSEID='$nurseID' where COMPLAINTID = $complaintID";
+				return $this->query($strQuery);
+		}
+
 	}
 ?>
