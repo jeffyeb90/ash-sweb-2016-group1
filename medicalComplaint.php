c2918f8ad284070bfdb17d1b31433847536d0581
Andrew Abbeyquaye <andrew.abbeyquaye@icloud.com>
2016-04-26 18:35:02 +0000

    Changes made to queries correct errors on the remote server

--- medicalComplaint.php
+++ medicalComplaint.php
@@ -28,7 +28,7 @@
       /**
       *@var string $strQuery Contains sql statement
       */
-      $strQuery="insert into medicalcomplaint set
+      $strQuery="insert into medicalComplaint set
   						STUDENTID=$studentID,
   						DATE='$date',
   						TEMPERATURE=$temperature,
@@ -72,7 +72,7 @@
 		**/
 
 		function getComplaints($filter=false){
-			 $strQuery="select COMPLAINTID, STUDENTID, DATE, TEMPERATURE, SYMPTOMS, diseases.NAME, CAUSE, PRESCRIPTION, nurses.FIRSTNAME, nurses.LASTNAME from medicalcomplaint left join diseases on medicalcomplaint.DIAGNOSIS=diseases.DISEASEID left join nurses on  medicalComplaint.NURSEID=nurses.NURSEID";
+			 $strQuery="select COMPLAINTID, STUDENTID, DATE, TEMPERATURE, SYMPTOMS, diseases.NAME, CAUSE, PRESCRIPTION, nurses.FIRSTNAME, nurses.LASTNAME from medicalComplaint left join diseases on medicalComplaint.DIAGNOSIS=diseases.DISEASEID left join nurses on  medicalComplaint.NURSEID=nurses.NURSEID";
 
 
 
@@ -107,12 +107,12 @@
 
 
 		function updateComplaint($complaintID, $studentID, $temperature, $symptoms, $diagnosis, $cause, $prescription, $nurseID){
-				$strQuery="Update medicalcomplaint set STUDENTID=$studentID,  TEMPERATURE=$temperature, SYMPTOMS='$symptoms', DIAGNOSIS='$diagnosis', CAUSE='$cause', PRESCRIPTION='$prescription', NURSEID='$nurseID' where COMPLAINTID = $complaintID";
+				$strQuery="Update medicalComplaint set STUDENTID=$studentID,  TEMPERATURE=$temperature, SYMPTOMS='$symptoms', DIAGNOSIS='$diagnosis', CAUSE='$cause', PRESCRIPTION='$prescription', NURSEID='$nurseID' where COMPLAINTID = $complaintID";
 				return $this->query($strQuery);
 		}
 
 		function getComplaintByID($complaintID){
-			$strQuery="select COMPLAINTID, STUDENTID, DATE, TEMPERATURE, SYMPTOMS, DIAGNOSIS, CAUSE, PRESCRIPTION, NURSEID from medicalcomplaint where COMPLAINTID=$complaintID";
+			$strQuery="select COMPLAINTID, STUDENTID, DATE, TEMPERATURE, SYMPTOMS, DIAGNOSIS, CAUSE, PRESCRIPTION, NURSEID from medicalComplaint where COMPLAINTID=$complaintID";
 			return $this->query($strQuery);
 		}
 
